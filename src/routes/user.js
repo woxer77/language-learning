2102269c511cd7f008334c88eb33ec976c14ce1a
Denis Ohrimenko <woxer77@gmail.com>
2024-04-22 15:16:01 +0300

    fix: fixed getUserById route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const UserController = require("../controllers/user-controller");
 
-router.get("/user/:id", UserController.getUserById);
+router.get("/:id", UserController.getUserById);
 
 module.exports = router;
