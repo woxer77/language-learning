c146629867edf9480a361f47b881b533d7884590
Denis Ohrimenko <woxer77@gmail.com>
2024-04-24 18:24:14 +0300

    feat: removed important credentials from auth-service payload

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -5,25 +5,13 @@ const tokenService = require("../services/token-service");
 
 const ApiError = require("../exceptions/api-error");
 
-const config = require("../configs/config");
-
 module.exports = {
   async registration(user) {
     try {
       const candidate = await userDbService.getUserByEmail(user.email);
       if (candidate) return ApiError.BadRequest('The user with provided email does already exist');
 
-
       const hashedPassword = await hash(user.password, 10);
-      /*const hashedPassword = await hash(user.password, Number(config.SALT_ROUNDS), function(err, hash) {
-        if (err) { throw (err); }
-
-        compare(user.password, hash, function(err, result) {
-          if (err) { throw (err); }
-          console.log(result);
-          return hash;
-        });
-      });*/
 
       const userId = await userDbService.createUser({
         email: user.email,
@@ -31,8 +19,7 @@ module.exports = {
       });
 
       const userPayload = {
-        userId: userId,
-        email: user.email
+        userId: userId
       };
       const { accessToken, refreshToken } = tokenService.generateTokens(userPayload);
       await tokenService.saveToken(userId, refreshToken);
@@ -47,7 +34,7 @@ module.exports = {
     if (!user) {
       throw ApiError.BadRequest('User with this email was not found.');
     }
-    console.log(password, user.password);
+
     const isPasswordsEqual = await compare(password, user.password);
     if (!isPasswordsEqual) {
       throw ApiError.BadRequest('Incorrect password.');
@@ -80,8 +67,7 @@ module.exports = {
     const user = await userDbService.getUserById(tokenFromDb.user_id);
 
     const userPayload = {
-      userId: user.user_id,
-      email: user.email
+      userId: user.user_id
     };
     const tokens = tokenService.generateTokens(userPayload);
     await tokenService.saveToken(user.user_id, tokens.refreshToken);
