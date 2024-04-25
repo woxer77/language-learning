b2a17d91ab8c4ecb19f0666c5fd6a5306778056a
Denis Ohrimenko <woxer77@gmail.com>
2024-04-25 14:06:24 +0300

    fix#2: fixed setCookie export into function

--- src/helpers/setCookie.js
+++ src/helpers/setCookie.js
@@ -1,7 +1,7 @@
 const tokenTimelineToMs = require("src/helpers/tokenTimelineToMs");
 const config = require("src/configs/config");
 
-module.exports = setCookie = (res, data) => {
+module.exports = function setCookie (res, data) {
   const refreshMaxAge = tokenTimelineToMs(config.JWT_REFRESH_EXPIRES);
   const hasSSL = config.DEVELOPMENT_STAGE === 'production'; // if SSL certificate is valid
   const sameSite = config.DEVELOPMENT_STAGE === 'production' ? 'none' : 'strict';
