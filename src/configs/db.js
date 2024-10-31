806e7c9215427b45172193f23627b9550f4f7f95
Denis Ohrimenko <woxer77@gmail.com>
2024-10-31 17:30:07 +0200

    fix#2: updated db config

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const config = require('../configs/config');
 
 module.exports = require('knex')({
@@ -9,11 +8,6 @@ module.exports = require('knex')({
     user: config.DB_USER,
     password: config.DB_PASSWORD,
     database: config.DB_DATABASE,
-    ssl: {
-      rejectUnauthorized: false,
-      ca: fs.readFileSync(config.DB_CA_CERT).toString(),
-      key: fs.readFileSync(config.DB_CLIENT_KEY).toString(),
-      cert: fs.readFileSync(config.DB_CLIENT_CERT).toString()
-    }
+    ssl: true
   }
 });
